cc8fd5b9286442575678704c150430334dab4de9
Alena <aln.evskv.1@mail.ru>
2022-10-05 13:36:31 +0500

    add task2.0

--- task2/Program.cs
+++ task2/Program.cs
@@ -7,8 +7,8 @@ Console.WriteLine ( "Введите второе число" );
 int number2 = int.Parse(Console.ReadLine());
 if (number1>number2)
 {
-Console.WriteLine( "Max number1" );
+Console.WriteLine( "Max " + number1 );
 }
 else 
-Console.WriteLine( "Max number2" );
+Console.WriteLine( "Max " + number2 );
 
